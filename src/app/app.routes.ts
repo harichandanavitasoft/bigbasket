f6e0014097b05a3e4cce3991862ddb0028a768e2
harichandanavitasoft <harichandanavita@gmail.com>
2024-05-02 14:56:13 +0530

    home page  design completed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,15 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/admin/login/login.component';
+import { LandingComponent } from './pages/website/landing/landing.component';
+import { HomeComponent } from './pages/website/home/home.component';
 
 
 export const routes: Routes = [
+  
+    {path:'',component:LandingComponent ,children:[
+        {path:'',component:HomeComponent}
+
+    ]}
+ 
     
 ];
--- src/app/pages/website/landing/landing.component.ts
+++ src/app/pages/website/landing/landing.component.ts
@@ -1,9 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,RouterLink,RouterOutlet],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.css'
 })
